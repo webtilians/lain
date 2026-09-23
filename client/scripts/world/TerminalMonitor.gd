df1648ae55b64410bd057611e9282d4387c52408
enrikelr <enrikelr@hotmail.com>
2026-09-23 19:38:55 +0200

    feat(prologue): route apartment computer to DOS interface until real terminal puzzle solved

--- client/scripts/world/TerminalMonitor.gd
+++ client/scripts/world/TerminalMonitor.gd
@@ -1,6 +1,12 @@
 extends StaticBody3D
 
 func interact() -> void:
+	var prologue: Dictionary = WorldApi.snapshot.get("prologue", {})
+	if bool(prologue.get("enabled", false)) and str(
+		prologue.get("stage", "")
+	) != "CONNECTED":
+		PrologueTerminal.open_terminal()
+		return
 	var terminal = get_tree().get_first_node_in_group("terminal_ui")
 	if terminal == null:
 		return
